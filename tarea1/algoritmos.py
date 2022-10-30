bdd4127e14a003031791ccf608ee344865721441
Aquiles Viza <aquiles.viza@sansano.usm.cl>
2022-10-30 18:52:26 -0300

    Agregué funciones de generación de puntos

--- tarea1/algoritmos.py
+++ tarea1/algoritmos.py
@@ -4,4 +4,17 @@ def hola():
 
 def hola2():
   print ("hola")
-  
+
+import random
+import numpy as np
+
+def pointGenerator():
+  return np.array([
+    random.uniform(-10, 10),
+    random.uniform(-20, 20),
+  ])
+
+def instanceGenerator():
+  return np.array([
+    pointGenerator() for _ in range(10)
+  ])
