3c0ebcd0ae721654e523a07e9f4a030d1024b812
Aquiles Viza <aquiles.viza@sansano.usm.cl>
2022-10-31 18:46:00 -0300

    Splitted

---
+++ tarea.py
@@ -0,0 +1,34 @@
+from tarea1.algoritmos import *
+from tarea1.graficos import *
+
+import numpy as np
+
+
+def f(X):
+  x1 = X[0]
+  x2 = X[1]
+
+  return (4 - 2.1 * x1**2 + (x1**4)/3) * x1**2 + x1*x2 + (-4 + 4*x2**2) * x2**2
+
+
+def fd(X):
+  x1 = X[0]
+  x2 = X[1]
+
+  return np.array([
+    2 * (x1**5 - 4.2 * x1**3 + 4 * x1 + 0.5 * x2),
+    x1 + 16*x2**3 - 8*x2
+  ])
+
+
+initial_state = State(
+  np.array([0,0]),
+  alpha=0.3,
+  beta=0.6,
+  f=f,
+  fd=fd
+)
+
+results: Capture = metodo_gradiente(initial_state)
+
+#print(f([0,0]))
--- tarea1/algoritmos.py
+++ tarea1/algoritmos.py
@@ -1,3 +1,12 @@
+import random
+import numpy as np
+from dataclasses import dataclass
+from typing import Any, Callable
+
+
+import numpy as np
+
+
 def hola():
   print ("hola")
 
@@ -5,8 +14,6 @@ def hola():
 def hola2():
   print ("hola")
 
-import random
-import numpy as np
 
 def pointGenerator():
   return np.array([
@@ -18,3 +25,73 @@ def instanceGenerator():
   return np.array([
     pointGenerator() for _ in range(10)
   ])
+
+
+
+
+@dataclass
+class State:
+  punto: Any
+  alpha: float
+  beta: float
+  f: Callable[..., Any]
+  fd: Callable[..., Any]
+
+
+@dataclass
+class Capture:
+  k: int
+  FO_optima: list
+
+
+def state_update(state: State):
+  punto = state.punto
+  alpha = state.alpha
+  beta = state.beta
+  f = state.f
+  fd = state.fd
+
+  t = 1.0
+
+  while True:
+    LR = f(punto) + alpha * t * np.dot(np.transpose(fd(punto)), - fd(punto))
+    LL = f(punto - t*fd(punto))
+    t = t * beta
+
+    if LR>LL:
+      state.punto = punto - t * fd(punto)
+      break
+
+
+def capture_update(state: State, capture: Capture) -> Capture:
+
+  punto = state.punto
+  f = state.f
+
+  capture.k += 1
+  capture.FO_optima.append(f(punto))
+
+  if capture.k % 1000 == 0:
+    print("hola")
+
+
+def metodo_gradiente(state: State, epsilon=0.001):
+  
+  capture = Capture(k=0, FO_optima=list())
+
+  f = state.f
+  fd = state.fd
+
+  while True:
+    state_update(state)
+
+    if np.abs(np.linalg.norm(fd(state.punto))) <= epsilon:
+      break
+
+    capture_update(state, capture)
+
+
+  capture_update(state, capture)
+  print(f(state.punto), capture.k)
+  
+  return capture
